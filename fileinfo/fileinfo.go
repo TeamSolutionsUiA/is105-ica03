5dcfbd1c8876cad159531f18cf4c3ea272b25cac
Are <arefjerm@gmail.com>
2019-03-11 13:17:25 +0100

    Oppgave2V2

--- fileinfo/fileinfo.go
+++ fileinfo/fileinfo.go
@@ -7,39 +7,41 @@ import (
 )
 
 func main() {
-	file, err := os.Lstat("../files/text1.txt")
+	filename := os.Args[1]
+	file, err := os.Lstat(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	appendhex:="2d72772d72772d72772d";
 	fileMode := file.Mode()
 	fileSize := file.Size()
+	fileSizeB := fileSize / 8
+	fileSizeKiB := fileSizeB / 1024
+	fileSizeMiB := fileSizeKiB / 1024
+	fileSizeGiB := fileSizeMiB / 1024
 	isDir := fileMode.IsDir()
 	isReg := fileMode.IsRegular()
 	permissionBits := fileMode.Perm()
-	isAppendOnly :=false;
-	if (fmt.Sprintf("%x",permissionBits)==appendhex){
-		isAppendOnly=true;
-
+	isAppendOnly := fileMode&os.ModeAppend != 0
+	isDevice := false
+	isUnixChar := false
+	isUnixBloc := false
+	if fileMode&os.ModeDevice != 0 {
+		isDevice = true
+		if fileMode&os.ModeCharDevice != 0 {
+			isUnixChar = true
+		} else {
+			isUnixBloc = true
+		}
 	}
-	
-	
+	isSymbolicLink := fileMode&os.ModeSymlink != 0
 
-	//isDeviseFile :=
-	
-	//isUnixChar :=
-	
-	//isUnixBloc :=
-	
-	//isSymbolicLink :=
-	
-	fmt.Print("filesize byte ", fileSize, )
-	fmt.Print("IS Dir ", isDir)
-	fmt.Print("Is Reg ", isReg )
-	fmt.Print("Uinx code ", permissionBits) 
-	fmt.Print("is append only ", isAppendOnly)
-
-
-	
-	
+	fmt.Printf("filesize: %d bytes, %d KiB, %d MiB, %d GiB\n", fileSizeB, fileSizeKiB, fileSizeMiB, fileSizeGiB)
+	fmt.Println("IS Dir ", isDir)
+	fmt.Println("Is Reg ", isReg)
+	fmt.Println("Uinx code ", permissionBits)
+	fmt.Println("is append only: ", isAppendOnly)
+	fmt.Println("is device file: ", isDevice)
+	fmt.Println("is Unix character device: ", isUnixChar)
+	fmt.Println("is Unix block device", isUnixBloc)
+	fmt.Println("is symbolic link", isSymbolicLink)
 }
