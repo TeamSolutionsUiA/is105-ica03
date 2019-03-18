4a1c53a2392e886bffcc1658d40cbf916643e8f4
Are <arefjerm@gmail.com>
2019-03-18 11:44:31 +0100

    5A

--- TCP/Klient/TCP-Klient.go
+++ TCP/Klient/TCP-Klient.go
@@ -1,23 +1,15 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
 func main() {
 
 	// connect to this socket
 	conn, _ := net.Dial("tcp", "127.0.0.1:5000")
-	for {
-		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
-		// send to socket
-		fmt.Fprintf(conn, text+"\n")
-		// listen for reply
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: " + message)
-	}
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Print("Message from server: " + message)
 }
